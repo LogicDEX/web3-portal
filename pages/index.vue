09a6ac8b206544a7aeeafc753e5f8bf1e6688c72
cartosys <plaster1@gmail.com>
2020-01-26 20:15:06 -0500

    moved putTxHash in appropriate spot along the process

--- pages/index.vue
+++ pages/index.vue
@@ -275,7 +275,6 @@ export default {
     },
     onWithdrawComplete: function() {
       this.showConfirmWithdrawComplete = false
-      this.withdrawalHash = this.withdrawReceipt.hash
     },
     async putTxHash(hash, network) {
       var webDataUrl =
@@ -902,7 +901,8 @@ export default {
       )
 
       console.log('Approving Loom Transfer Gateway to take the coins.')
-      await this.loomToken.approve(dAppChainGatewayAddr, amountInWei)
+      var res = await this.loomToken.approve(dAppChainGatewayAddr, amountInWei)
+      this.putTxHash(res.hash, 'loom')
       const timeout = 60 * 5000
       const loomCoinContractAddress = this.LOOM_CONTRACT_ADDR
       const tokenAddress = Address.fromString(
@@ -942,18 +942,17 @@ export default {
           listener
         )
       })
-      res = await gatewayContract.withdrawERC20Async(
+
+      await gatewayContract.withdrawERC20Async(
         new BN(amountInWei, 10),
         tokenAddress,
         ownerMainnetAddr
       )
-      console.log(res)
-      this.putTxHash(res.txHash, 'loom')
       // alert('Please wait a few minutes for the next prompt.') //Modal 5 -- show a status bar while waiting
       // alert(this.busy)
       this.showTransferStatus = true
       this.busy = true
-      return await receiveSignedWithdrawalEvent
+      await receiveSignedWithdrawalEvent
     }
   }
 }
